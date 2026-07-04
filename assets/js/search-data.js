5e9081497eefe0a9a28456b7382723761ec9096d
lilyzhang183 <39612198+lilyzhang183@users.noreply.github.com>
2026-07-04 03:53:29 +0000

    Deploying to gh-pages from @ lilyzhang183/lilyzhang183.github.io@aa3b3c564bd0ffbb0d999fcbf0dc3381576a67e3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "Teaching and mentoring experience of Lily Zhang in mathematics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
