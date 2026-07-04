fd27ca4b185240f80a185c2368dfbf96a283e58a
lilyzhang183 <39612198+lilyzhang183@users.noreply.github.com>
2026-07-04 03:54:03 +0000

    Deploying to gh-pages from @ lilyzhang183/lilyzhang183.github.io@496e34d7d1d2e9ee8e5c76b8cff6247b72deecdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "Curriculum vitae of Lily Zhang.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
